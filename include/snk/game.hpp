5313dbc14d6f1b3861b43ccde3c7e7d35174a8df
Christoph Malek <malekchristoph@gmail.com>
2013-12-17 19:25:56 +0100

    adapted game

--- include/snk/game.hpp
+++ include/snk/game.hpp
@@ -9,6 +9,7 @@
 
 #include "debug_text.hpp"
 #include "fruit_handler.hpp"
+#include "menu.hpp"
 #include "snake.hpp"
 
 #include <mlk/time/time.h>
@@ -19,6 +20,15 @@
 
 namespace snk
 {
+	struct difficulty_values
+	{
+		static constexpr mlk::ullong m_easy_move_interval{100};
+		static constexpr mlk::ullong m_easy_spawn_interval{5000};
+
+		static constexpr mlk::ullong m_hard_move_interval{10};
+		static constexpr mlk::ullong m_hard_spawn_interval{500};
+	};
+
 	class game
 	{
 		sf::RenderWindow m_window;
@@ -26,6 +36,7 @@ namespace snk
 		snake m_snake_player;
 		fruit_handler m_fh;
 		debug_text m_debug_text{"consola.ttf"};
+		menu m_menu;
 
 		bool m_running{false};
 		bool m_paused{false};
@@ -35,7 +46,10 @@ namespace snk
 		game(const sf::VideoMode& size, const std::string& title) :
 			m_window{size, title},
 			m_snake_player{{size.width / 2.f, size.height / 2.f}}
-		{m_snake_player.m_on_self_hit = [this]{this->stop();};}
+		{
+			m_snake_player.m_on_self_hit = [this]{this->stop();};
+			m_menu.m_on_choose = [this](difficulty_modes mode){this->menu_choose(mode);};
+		}
 
 		int run()
 		{
@@ -44,9 +58,7 @@ namespace snk
 			{
 				auto start(mlk::tm::time_pnt());
 
-				this->update_events();
-				this->update_game();
-				this->render();
+				this->update();
 
 				auto end(mlk::tm::time_pnt());
 				auto duration(mlk::tm::duration_as<float>(start, end));
@@ -63,6 +75,23 @@ namespace snk
 		{return m_paused;}
 
 	private:
+		void menu_choose(difficulty_modes mode) noexcept
+		{
+			switch (mode)
+			{
+			case difficulty_modes::easy:
+				m_snake_player.set_move_interval(difficulty_values::m_easy_move_interval);
+				m_fh.set_spawn_interval(difficulty_values::m_easy_spawn_interval);
+				break;
+			case difficulty_modes::normal: break;
+			case difficulty_modes::hard:
+				m_snake_player.set_move_interval(difficulty_values::m_hard_move_interval);
+				m_fh.set_spawn_interval(difficulty_values::m_hard_spawn_interval);
+			default:
+				break;
+			}
+		}
+
 		void update_events() noexcept
 		{
 			sf::Event e;
@@ -76,6 +105,18 @@ namespace snk
 					case sf::Keyboard::D: m_debug = !m_debug; break;
 					default: break;
 					}
+		}
+
+		void update()
+		{
+			this->update_events();
+
+			if(m_menu.is_active())
+				m_menu.update();
+			else
+				this->update_game();
+
+			this->render();
 		}
 
 		void update_game()
@@ -92,14 +133,20 @@ namespace snk
 		{
 			m_window.clear();
 
-			for(const auto& body_part : m_snake_player.body())
-				m_window.draw(body_part);
+			if(!m_menu.is_active())
+			{
+				for(const auto& body_part : m_snake_player.body())
+					m_window.draw(body_part);
 
-			for(const auto& fruit : m_fh.fruits())
-				m_window.draw(fruit);
+				for(const auto& fruit : m_fh.fruits())
+					m_window.draw(fruit);
+			}
 
 			if(m_debug)
 				m_window.draw(m_debug_text.drawable());
+
+			if(m_menu.is_active())
+				m_window.draw(m_menu.drawable());
 
 			m_window.display();
 		}
